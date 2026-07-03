be0b020c928cab4e2c424398f7478cb117b2603a
jjul0i <josesilva47@estudante.ufscar.br>
2026-07-03 09:33:42 -0300

    Update top.v

--- top.v
+++ top.v
@@ -1,12 +1,32 @@
 module top (
     input [31:0] in, 
     input [1:0] sel, 
-    output [7:0] out);
+    output [7:0] out
+);
 
-    //declare os fios intermediarios 
+wire [7:0] w1, w2;
 
-    // instancie os muxes que precisar 
+// Primeiro nível de muxes
+mux2byte m1 (
+    .a(in[7:0]),
+    .b(in[15:8]),
+    .sel(sel[0]),
+    .out(w1)
+);
 
+mux2byte m2 (
+    .a(in[23:16]),
+    .b(in[31:24]),
+    .sel(sel[0]),
+    .out(w2)
+);
 
+// Segundo nível
+mux2byte m3 (
+    .a(w1),
+    .b(w2),
+    .sel(sel[1]),
+    .out(out)
+);
 
 endmodule
